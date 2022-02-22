390b6cf66aa81c9c61c12a2ad135e0b4bb273fd2
Tomás de Camino Beck, Ph.D <tomas.decamino@gmail.com>
2022-02-22 08:19:22 -0600

    Add files via upload

---
+++ Circuit_Playground_Basic.ino
@@ -0,0 +1,19 @@
+//Universidad Cenfotec
+//Escuela de Sistemas Inteligentes
+//Curso Introducción a Tecnologías de Información
+//Tomás de Camino Beck, Ph.D.
+
+//Carga la librería de Adafruit CP
+#include <Adafruit_Circuit_Playground.h>
+
+void setup() {
+  //Inicia el Circuit Playground
+  CircuitPlayground.begin();
+}
+
+
+void loop() {
+  //escribir código que se repite en el CP
+
+
+}
