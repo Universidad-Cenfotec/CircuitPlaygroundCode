09dc5dfeca3ed66e6e65cec1dd2936da82a772a8
Tomás de Camino Beck, Ph.D <tomas.decamino@gmail.com>
2022-02-21 19:40:17 -0600

    Add files via upload

---
+++ Ejemplo_Serial_SensorLuz_Neo.ino
@@ -0,0 +1,27 @@
+//Universidad Cenfotec
+//Curso Introducción a Tecnologías de Información
+// Tomás de Camino Beck
+
+//Carga la librería de Adafruit CP
+#include <Adafruit_Circuit_Playground.h>
+
+void setup() {
+  //Inicia el Circuit Playground
+  CircuitPlayground.begin();
+  Serial.begin(9600);
+}
+
+
+void loop() {
+  //
+  int val = CircuitPlayground.lightSensor();
+  int brillo = map(val, 15,970, 100, 0);
+  CircuitPlayground.setBrightness(brillo);
+  CircuitPlayground.setPixelColor(5,255,255,255);
+  CircuitPlayground.setPixelColor(6,255,255,255);
+  CircuitPlayground.setPixelColor(7,255,255,255);
+  CircuitPlayground.setPixelColor(8,255,255,255);
+  CircuitPlayground.setPixelColor(9,255,255,255);
+ 
+
+}
